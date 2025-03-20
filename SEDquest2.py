ea0ecb907b12e84ca383d5a148f8099550eb32f0
Kazuma Ishio <kazumaishio@users.noreply.github.com>
2025-03-20 14:02:23 +0100

    SEDquest2: Residual plot base

---
+++ SEDquest2.py
@@ -0,0 +1,82 @@
+
+
+# plan
+# *******************
+# ***** General idea:
+# *******************
+# 
+# ** In what basis ** 
+# Now the evaluation is considered in significance level. 
+# Data points are simulated as a normalized value
+#  with respect to the standard deviation, 
+#  from probability distribution of a normal distribution. 
+# 
+# ** What is anomaly ** 
+# Strength of the spectral anomaly is interepreted as 
+# the expected normalized value. This is stronger(higher), when
+#   - the anomaly is strong
+#   - the base SED has smaller error bars, due to 
+#       a strong spectrum, or a good observation condition. 
+# 
+# ** 
+# In many observations, the signal is stronger or weaker. 
+#  I want to evaluate the trend of the "total" significance 
+#  as a function of 
+#   - binning
+#   - strength of the signal
+#   - width of the signal
+#   - strength of background 
+#   - fluctuation of those above. 
+# 
+# I want to obtain a clear understanding of significance expectation,
+# for a given characteristic of the source/observation, 
+# characterised by the above parameters. 
+#   
+# Toy model of SED
+#  (** Null hypothesis **) 
+# - N bins 
+# - N consecutive bins to evaluate  
+# - N observations
+# 
+# 
+#  (** Alternative model **)
+# - peak info
+#   - peak level in sigma
+#   - width 
+#  
+###############
+# Look elsewhere effect
+#  p' = 1- (1-p)^N
+# p for WHAT? needs to be corrected for N times of WHAT? 
+#
+
+#  
+# N bins with 
+# #
+
+
+# 1. Generate a toy model of SED
+# 1-1. 
+# Observation: an array of N significances 
+# Observations: an array of Observation 
+
+
+
+# 1-2. 
+# Probability distribution of peak detection in Observation 
+# -> 3 consecutive bins in 11 bins to be 3%? 
+#  
+
+
+
+
+# 2. Generate a toy model of SED with a peak
+# 
+# 
+
+
+# 3. Evaluate the significance of the peak
+# 
+# 
+
+# 4.  
